43da4b1e2ff063ef5a676fedec889128cee62647
Dmitriy Rovenskiy <arhitec@bk.ru>
2020-11-25 22:11:42 +0300

    v1.0

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -4,7 +4,7 @@ export default function ProjectList(props) {
     const { projects } = props;
     return (
     <div className="projectlist">
-        {projects.map(o => <img src={o.img} key={performance.now()}/>)}
+        {projects.map(o => <img src={o.img} alt='pic' key={performance.now()}/>)}
     </div> /*{performance.now() просто уникальный инжификатор}*/
     )
 };
